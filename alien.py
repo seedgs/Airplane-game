c3e5bff38a50b82e11652f007d3d4b2df6c66035
huanghui <seedgs@qq.com>
2021-07-21 19:07:31 +0800

    alien of image modify route

--- alien.py
+++ alien.py
@@ -9,7 +9,7 @@ class Alien(Sprite):
         self.ai_settings = ai_settings
 
         # 加载外星人图像
-        self.image = pygame.image.load('/Users/huanghui/python_work/Airplane-game-python-/images/alien.bmp')
+        self.image = pygame.image.load('/Users/huanghui/python_work/Airplane-game/images/alien.bmp')
         # 设置其rect属性
         self.rect = self.image.get_rect()
 
